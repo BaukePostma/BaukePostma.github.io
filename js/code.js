8b232211de463f71677061a936d430a990985d2c
BaukePostma <bauke.j.p@hotmail.com>
2017-06-09 22:24:33 +0200

    New Version
    Redid almost everything to use Bootstrap 3.0

---
+++ js/code.js
@@ -0,0 +1,36 @@
+$(document).ready(function(){
+console.log("Hello WOrld");	
+$("jumbotron").hide(4000);
+
+$('#nav-all').click(function(){
+	$('.gamedesign').show();
+	$('.3d').show();
+	$('.webdesign').show();
+});
+
+$('#nav-games').click(function(){
+	$('.gamedesign').show();
+	$('.3d').hide();
+	$('.webdesign').hide();
+
+});
+
+$('#nav-3d').click(function(){
+	$('.gamedesign').hide();
+	$('.3d').show();
+	$('.webdesign').hide();
+
+});
+
+$('#nav-webdesign').click(function(){
+	$('.gamedesign').hide();
+	$('.3d').hide();
+	$('.webdesign').show();
+
+
+});
+
+
+
+
+});
